ae3fff2c59c034375bf538857b5499352beecf21
Samuel Thiriot <samuel.thiriot.accounts@res-ear.ch>
2014-06-20 21:36:23 +0200

    algog GUI enhancements

---
+++ genlab.algog.core/src/genlab/algog/algos/flowtypes/GeneticTableFlowType.java
@@ -0,0 +1,33 @@
+package genlab.algog.algos.flowtypes;
+
+import genlab.core.model.meta.IFlowType;
+import genlab.core.model.meta.basics.flowtypes.TableFlowType;
+
+/**
+ * A table exactly similar to a standart table, 
+ * but with metadata related to a genetic algorithm
+ * 
+ * @author B12772
+ *
+ */
+public class GeneticTableFlowType extends TableFlowType {
+
+	public GeneticTableFlowType() {
+		super(
+				"algog.types.table",
+				"a table of values from algog",
+				"stores values in a table with metadata"
+				);
+	}
+
+	
+
+	@Override
+	public boolean compliantWith(IFlowType<?> other) {
+		return other.getId().equals(id) 
+				|| other.getId().equals(TableFlowType.SINGLETON.id);
+	}
+	
+	
+
+}
---
+++ genlab.algog.core/src/genlab/algog/algos/meta/IGenlabTableAlgogResult.java
@@ -0,0 +1,7 @@
+package genlab.algog.algos.meta;
+
+import genlab.core.model.meta.basics.flowtypes.IGenlabTable;
+
+public interface IGenlabTableAlgogResult extends IGenlabTable {
+
+}
