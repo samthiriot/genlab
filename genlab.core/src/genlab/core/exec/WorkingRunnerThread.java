9429d66592e37276f6bf4fed66bc6bfbca078e95
Alan Solon <alan.solon@edf.fr>
2015-05-18 10:39:19 +0200

    adding a recursive method for the crowded tournament operator

--- genlab.core/src/genlab/core/exec/WorkingRunnerThread.java
+++ genlab.core/src/genlab/core/exec/WorkingRunnerThread.java
@@ -78,7 +78,7 @@ public class WorkingRunnerThread extends Thread {
 					// if there is nothing, loop until something comes.
 					//messages.infoTech(this.getName()+" sleeping", getClass());
 					try {
-						Thread.sleep(100);
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
