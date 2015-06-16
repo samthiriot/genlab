4e76940ad78c1f862beef199609babdd53dbc778
Samuel Thiriot <samuel.thiriot.accounts@res-ear.ch>
2015-06-16 19:37:41 +0200

    added detection of column names for table

--- genlab.core/src/genlab/core/model/meta/basics/algos/AppendToTableInstance.java
+++ genlab.core/src/genlab/core/model/meta/basics/algos/AppendToTableInstance.java
@@ -1,13 +1,19 @@
 package genlab.core.model.meta.basics.algos;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import genlab.core.model.instance.AlgoInstance;
 import genlab.core.model.instance.IConnection;
 import genlab.core.model.instance.IGenlabWorkflowInstance;
 import genlab.core.model.instance.IInputOutputInstance;
+import genlab.core.model.instance.TextMessageFromAlgoInstance;
+import genlab.core.model.instance.WorkflowCheckResult;
 import genlab.core.model.meta.IAlgo;
 import genlab.core.model.meta.IInputOutput;
 import genlab.core.model.meta.basics.flowtypes.GenlabTable;
 import genlab.core.model.meta.basics.flowtypes.IGenlabTable;
+import genlab.core.usermachineinteraction.MessageLevel;
 
 @SuppressWarnings("serial")
 public class AppendToTableInstance extends AlgoInstance {
@@ -42,4 +48,28 @@ public class AppendToTableInstance extends AlgoInstance {
 		
 	}
 
+	@Override
+	public void checkForRun(WorkflowCheckResult res) {
+		super.checkForRun(res);
+		
+		// ensure there are not similar names in the column ? 
+		try {
+			IInputOutputInstance inputInstance = getInputInstanceForInput(AppendToTableAlgo.INPUT_ANYTHING);
+			Set<String> columnNames = new HashSet<String>(); 
+			for (IConnection c: inputInstance.getConnections()) {
+				if (!columnNames.add(c.getFrom().getName())) {
+					res.messages.add(new TextMessageFromAlgoInstance(
+							this, 
+							MessageLevel.ERROR, 
+							"several inputs of this table have for name "+c.getFrom().getName()+"; please rename one of the algorithms so there will be no conflict anymore"
+							)
+					);
+				}	
+			}
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
 }
