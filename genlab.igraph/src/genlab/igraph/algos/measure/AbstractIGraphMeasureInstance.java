3e251a7b1977c328dec6fa5f1fa8ecb5a0dcb8d5
Samuel Thiriot <samuel.thiriot.accounts@res-ear.ch>
2016-07-25 14:52:26 +0200

    igraph: added precomputed for graph generators

--- genlab.igraph/src/genlab/igraph/algos/measure/AbstractIGraphMeasureInstance.java
+++ genlab.igraph/src/genlab/igraph/algos/measure/AbstractIGraphMeasureInstance.java
@@ -3,6 +3,10 @@ package genlab.igraph.algos.measure;
 import genlab.core.model.instance.AlgoInstance;
 import genlab.core.model.instance.IGenlabWorkflowInstance;
 import genlab.core.model.meta.IAlgo;
+import genlab.core.model.meta.IInputOutput;
+import genlab.core.model.meta.basics.graphs.GraphDirectionality;
+import genlab.core.model.meta.basics.graphs.GraphFactory;
+import genlab.igraph.algos.generation.AbstractIGraphGenerator;
 
 public class AbstractIGraphMeasureInstance extends AlgoInstance {
 
@@ -22,6 +26,25 @@ public class AbstractIGraphMeasureInstance extends AlgoInstance {
 			IGenlabWorkflowInstance workflow) {
 		super(algo, workflow);
 		
+	}
+
+	@Override
+	public Object getPrecomputedValueForOutput(IInputOutput<?> output) {
+		
+		if (output == AbstractIGraphGenerator.OUTPUT_GRAPH) {
+		
+			return GraphFactory.createGraph(
+					"precomputed",
+					// TODO define directionnality based on parameters ?
+					GraphDirectionality.MIXED,
+					// TODO ?
+					false
+					);
+			
+		} else {
+			
+			return super.getPrecomputedValueForOutput(output);
+		}
 	}
 
 
